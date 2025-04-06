bc44ffa5bb273f79a1c8aa1316c0dcf289155cbd
HardBoss07 <matteo@bosshard.net>
2025-04-06 16:25:51 +0200

    if statement now fully implemented
    if statement is now fully implemented, also with correct indentation level so that the code is a little formatted

--- mat-compiler/output.rs
+++ mat-compiler/output.rs
@@ -1,6 +1,10 @@
 fn main() {
    let mut a = 10;
    if a < 10 {
-
+      println!("a is below 10. the value is {}", a);
+      a += 1;
+      if a < 8 {
+         println!("a is below 8. the value is {}", a);
+      }
    }
 }
--- mat-compiler/src/main.rs
+++ mat-compiler/src/main.rs
@@ -433,7 +433,7 @@ impl CodeGenerator {
         for node in self.ast.clone() {
             if let ASTNode::MainFunction(body) = node {
                 for inner_node in body {
-                    rust_code.push_str(&self.generate_node(&inner_node));
+                    rust_code.push_str(&self.generate_node(&inner_node, 1));
                 }
             }
         }
@@ -443,57 +443,61 @@ impl CodeGenerator {
         rust_code
     }
 
-    fn generate_node(&mut self, node: &ASTNode) -> String {
+    fn generate_node(&mut self, node: &ASTNode, indent_level: usize) -> String {
         match node {
             ASTNode::VariableDeclaration(name, value) => {
                 self.variables.insert(name.clone(), value.clone());
-                self.generate_variable_declaration(name, value)
+                self.generate_variable_declaration(name, value, indent_level)
             }
             ASTNode::VariableChangeValue(name, value) => {
                 self.variables.insert(name.clone(), value.clone());
-                self.generate_variable_change(name, value)
+                self.generate_variable_change(name, value, indent_level)
             }
             ASTNode::Operation(operator, identifier, value) => {
-                self.generate_operation(&operator.to_string(), identifier, value)
+                self.generate_operation(&operator.to_string(), identifier, value, indent_level)
             }
             ASTNode::Print(parts) => {
-                self.generate_print(parts)
+                self.generate_print(parts, indent_level)
             }
             ASTNode::IfStatement(condition, body_nodes) => {
-                self.generate_if(condition, body_nodes)
+                self.generate_if(condition, body_nodes, indent_level)
             }
             _ => String::new(),
         }
     }
 
-    fn generate_variable_declaration(&self, name: &str, value: &VariableType) -> String {
+    fn generate_variable_declaration(&self, name: &str, value: &VariableType, indent_level: usize) -> String {
+        let indent = "   ".repeat(indent_level);
         match value {
-            VariableType::Integer(v) => format!("   let mut {} = {};\n", name, v),
-            VariableType::Character(v) => format!("   let mut {} = '{}';\n", name, v),
-            VariableType::Float(v) => format!("   let mut {} = {};\n", name, v),
-            VariableType::Bool(v) => format!("   let mut {} = {};\n", name, v),
+            VariableType::Integer(v) => format!("{}let mut {} = {};\n", indent, name, v),
+            VariableType::Character(v) => format!("{}let mut {} = '{}';\n", indent, name, v),
+            VariableType::Float(v) => format!("{}let mut {} = {};\n", indent, name, v),
+            VariableType::Bool(v) => format!("{}let mut {} = {};\n", indent, name, v),
         }
     }
 
-    fn generate_variable_change(&self, name: &str, value: &VariableType) -> String {
+    fn generate_variable_change(&self, name: &str, value: &VariableType, indent_level: usize) -> String {
+        let indent = "   ".repeat(indent_level);
         match value {
-            VariableType::Integer(v) => format!("   {} = {};\n", name, v),
-            VariableType::Character(v) => format!("   {} = '{}';\n", name, v),
-            VariableType::Float(v) => format!("   {} = {};\n", name, v),
-            VariableType::Bool(v) => format!("   {} = {};\n", name, v),
+            VariableType::Integer(v) => format!("{}{} = {};\n", indent, name, v),
+            VariableType::Character(v) => format!("{}{} = '{}';\n", indent, name, v),
+            VariableType::Float(v) => format!("{}{} = {};\n", indent, name, v),
+            VariableType::Bool(v) => format!("{}{} = {};\n", indent, name, v),
         }
     }
 
-    fn generate_operation(&self, operator: &str, identifier: &str, value: &VariableType) -> String {
+    fn generate_operation(&self, operator: &str, identifier: &str, value: &VariableType, indent_level: usize) -> String {
+        let indent = "   ".repeat(indent_level);
         match value {
-            VariableType::Integer(v) => format!("   {} {}= {};\n", identifier, operator, v),
-            VariableType::Character(v) => format!("   {} {}= '{}';\n", identifier, operator, v),
-            VariableType::Float(v) => format!("   {} {}= {};\n", identifier, operator, v),
-            VariableType::Bool(v) => format!("   {} {}= {};\n", identifier, operator, v),
+            VariableType::Integer(v) => format!("{}{} {}= {};\n", indent, identifier, operator, v),
+            VariableType::Character(v) => format!("{}{} {}= '{}';\n", indent, identifier, operator, v),
+            VariableType::Float(v) => format!("{}{} {}= {};\n", indent, identifier, operator, v),
+            VariableType::Bool(v) => format!("{}{} {}= {};\n", indent, identifier, operator, v),
         }
     }
 
-    fn generate_print(&self, parts: &[PrintPart]) -> String {
+    fn generate_print(&self, parts: &[PrintPart], indent_level: usize) -> String {
+        let indent = "   ".repeat(indent_level);
         let mut format_string = String::new();
         let mut variables = Vec::new();
 
@@ -508,23 +512,24 @@ impl CodeGenerator {
         }
 
         format!(
-            "   println!(\"{}\", {});\n",
+            "{}println!(\"{}\", {});\n",
+            indent,
             format_string,
             variables.join(", ")
         )
     }
 
-    fn generate_if(&self, condition: &str, _body_nodes: &[ASTNode]) -> String {
-        let condition = format!("   if {} {{\n", condition);
-        let body = String::new();
+    fn generate_if(&mut self, condition: &str, _body_nodes: &Vec<ASTNode>, indent_level: usize) -> String {
+        let indent = "   ".repeat(indent_level);
+        let mut code = format!("{}if {} {{\n", indent, condition);
 
-        // TODO body generation
+        for node in _body_nodes {
+            code.push_str(&self.generate_node(&node, indent_level + 1));
+        }
 
-        format!(
-            "{}\n{}   }}\n",
-            condition,
-            body
-        )
+        code.push_str(&format!("{}}}\n", indent));
+        
+        code
     }
 }
 
